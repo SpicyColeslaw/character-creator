a19259325165721afcf8b01b9dff0976d86e0a0d
Joshua Cole <jcole@suretybonds.com>
2023-01-31 13:14:19 -0600

    complete 4 exercises

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+const perspectiveSquareStyles = {
+  width: "100%",
+  height: "40%",
+  backgroundColor: "hsl(195deg, 20%, 86%)",
+  position: "fixed",
+  bottom: 0,
+};
+
 function App() {
   return (
     <>
+      <div style={perspectiveSquareStyles}></div>
       <CharacterEditor />
       <Footer />
     </>
